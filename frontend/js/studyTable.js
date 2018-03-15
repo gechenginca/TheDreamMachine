43313e5b510b70825def60e9eb1de0c6a12650bb
Vladimir Efimov <vladimir.efimov@mail.utoronto.ca>
2018-03-15 12:39:13 -0400

    Fixed the misallignment in drawing

--- frontend/js/studyTable.js
+++ frontend/js/studyTable.js
@@ -8,8 +8,10 @@ document.addEventListener("DOMContentLoaded", function() {
    // get canvas element and create context
    var canvas  = document.getElementById('canvas');
    var context = canvas.getContext('2d');
-   var width   = window.innerWidth;
-   var height  = window.innerHeight;
+   //var width   = window.innerWidth;
+   //var height  = window.innerHeight;
+   var width = canvas.offsetWidth;
+   var height = canvas.offsetHeight;
    var socket  = io.connect();
 
    // set canvas to full browser width/height
@@ -23,8 +25,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
    canvas.onmousemove = function(e) {
       // normalize mouse position to range 0.0 - 1.0
-      mouse.pos.x = e.clientX / width;
-      mouse.pos.y = e.clientY / height;
+      mouse.pos.x = (e.pageX - canvas.offsetLeft ) / width;
+      mouse.pos.y = (e.pageY - canvas.offsetTop) / height;
       mouse.move = true;
    };
 
@@ -32,16 +34,24 @@ document.addEventListener("DOMContentLoaded", function() {
     socket.on('draw_line', function (data) {
       var line = data.line;
       context.beginPath();
-      context.moveTo(line[0].x * width, line[0].y * height);
-      context.lineTo(line[1].x * width, line[1].y * height);
+      var temp_x0 = (line[0].x * width);
+      var temp_y0 = line[0].y * height;
+      var temp_x1 = line[1].x * width;
+      var temp_y1 = line[1].y * height;
+
+      context.moveTo(temp_x0, temp_y0);
+      context.lineTo(temp_x1, temp_y1);
       context.stroke();
    });
    
    // main loop, running every 25ms
+   // TODO run on listener mouse down, stop running on mouse up
    function mainLoop() {
       // check if the user is drawing
       if (mouse.click && mouse.move && mouse.pos_prev) {
          // send line to to the server
+         var m_pos = mouse.pos;
+         var prev_m_pos = mouse.pos_prev;
          socket.emit('draw_line', { line: [ mouse.pos, mouse.pos_prev ] });
          mouse.move = false;
       }
